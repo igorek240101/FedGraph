432f363c57c6c6443bf660efc550b89f6261ffde
deadog911@gmail.com <deadog911@gmail.com>
2022-12-14 03:30:07 +0300

    Добавил класс Vertex и Path

---
+++ FedGraph/FedGraph.Main/Path.cs
@@ -0,0 +1,27 @@
+﻿namespace FedGraph.Main
+{
+    public class Path
+    {
+        public Path prev;
+        public Vertex vertex;
+        private int min_length;
+
+        public Path(Vertex vertex, int min_length, Path prev)
+        {
+            this.vertex = vertex;
+            this.min_length = min_length;
+            this.prev = prev;
+        }
+
+        public int getMinLength()
+        {
+            return this.min_length;
+        }
+
+        public void setPrevious(Path prev, int edgeWeight)
+        {
+            this.prev = prev;
+            this.min_length = prev.getMinLength() + edgeWeight;
+        }
+    }
+}
