dd37f7422e1e9222d368f25c306013ba38fb9479
GLeb B <Glebbot@inbox.ru>
2022-12-21 00:14:28 +0300

    Автотесты Path Vertex

---
+++ FedGraph/FedGraph.MainTests/PathTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FedGraph.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FedGraph.Main.Tests
+{
+    [TestClass()]
+    public class PathTests
+    {
+        private Path prev;
+        private Vertex TestVertex;
+        [TestInitialize]
+        public void Init() {
+            TestVertex = new Vertex(0, "0");
+            prev = new Path(TestVertex, 0, null);
+        }
+        [TestMethod()]
+        public void PathTest()
+        {
+            int expected = 10;
+            int newWiight = 10;
+            Vertex NewVertex = new Vertex(1, "1");
+            prev = new Path(NewVertex, prev.getMinLength()+newWiight, prev);
+            Assert.AreEqual(expected, prev.getMinLength());
+        }
+    }
+}
---
+++ FedGraph/FedGraph.MainTests/VertexTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FedGraph.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FedGraph.Main.Tests
+{
+    [TestClass()]
+    public class VertexTests
+    {
+        private Vertex TestVertex;
+        [TestInitialize]
+        public void Init() {
+            int id = 0;
+            string info = "Test";
+            TestVertex = new Vertex(id,info);
+
+        }
+
+        [TestMethod()]
+        public void addAdjVertexTest_addone()
+        {
+            int expect_id = 0;
+            Vertex NewVertex = new Vertex(0, "New");
+            TestVertex.addAdjVertex(NewVertex);
+            Vertex actual = TestVertex.getAdjVertex(expect_id);
+            Assert.AreEqual(NewVertex, actual);
+        }
+        [TestMethod()]
+        public void addAdjVertexTest_addmany()
+        {
+            for (int i = 0; i<=100; i++)
+            {
+                Vertex NewVertex = new Vertex(i, "New");
+                TestVertex.addAdjVertex(NewVertex);
+                Vertex actual = TestVertex.getAdjVertex(i);
+                Assert.AreEqual(NewVertex, actual);
+            }
+            
+        }
+    }
+}
